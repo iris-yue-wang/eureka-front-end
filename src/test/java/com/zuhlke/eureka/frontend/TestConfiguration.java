ff5f5f92ec4eac5c9f131f277022830bfda82020
Wang, Yue <Yue.Wang@zuhlke.com>
2019-05-08 13:14:09 +0100

    Added tests

--- src/test/java/com/zuhlke/eureka/frontend/TestConfiguration.java
+++ src/test/java/com/zuhlke/eureka/frontend/TestConfiguration.java
@@ -4,8 +4,6 @@ import com.zuhlke.eureka.frontend.connectors.Connector;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpServerErrorException;
 
 @SpringBootConfiguration
 @Profile("test")
@@ -14,7 +12,7 @@ public class TestConfiguration {
     @Bean
     public Connector testConnector() {
         return () -> {
-            throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new RuntimeException();
         };
     }
 }
